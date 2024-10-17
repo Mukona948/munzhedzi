a0d2b74790a96b1d48e7f53c798ae25201ec7081
Mukona948 <themelimukona@gmail.com>
2024-10-17 21:52:17 +0200

    added path to upload images

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE) 
-    image = models.ImageField(default='profile_pic.jpg',upload_to='profile_pictures'),
+    image = models.ImageField(default='profile_pic.jpg',upload_to='profile_pictures')
     location = models.CharField(max_length=500)
     
     def __str__(self):
