0bcd755dc6601eaeb1338728d736139d9ed68f54
Mukona948 <themelimukona@gmail.com>
2024-10-17 20:21:12 +0200

    added a profile template in users app,also added  css stylish in base.html

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('food/',include('food.urls')),
     path('register/',user_views.register,name='register'),
     path('login/',authentication_views.LoginView.as_view(template_name ='users/login.html'),name='login'),
-    path('logout/',authentication_views.LogoutView.as_view(template_name='users/logout'),name='logout')
+    #path('logout/',authentication_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('logout/', user_views.logout_view, name='logout'),
+    path('profile/',user_views.profile_page,name='profile'),
 ]
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , redirect
 from django.contrib import messages 
 from .forms import RegisterForm 
-
+from django.contrib.auth import logout
 
 # Create your views here.
 
@@ -12,10 +12,17 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Welcome {username} , your account is created')
+            messages.success(request,f' hello {username} successfully logged in ')
             return redirect('login')
     
     else:
         form =RegisterForm() 
     
     return render(request,'users/register.html',{'form':form})
+
+def logout_view(request):
+        logout(request)
+        return render(request, 'users/logout.html')
+    
+def profile_page(request):
+    return render(request,'users/profile.html')
