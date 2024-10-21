cfdf64cf1c2c7d2f518ec26e77a4e165f235d06e
Mukona948 <themelimukona@gmail.com>
2024-10-21 16:48:32 +0200

    updates to food app

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,19 +1,17 @@
-from . import views 
 from django.urls import path
+from . import views
 
 app_name = 'food'
 urlpatterns = [
-    #food path
-    path('',views.index,name='index'),   
-    #food 1 path
-    path('<int:item_id>/',views.details,name='details'),
-    path('item/',views.item,name='item'),
-    # add items
-    path('add',views.create_item,name='create_item'),
-    #edit items as user
-    path('update/<int:id>/',views.update_item,name='update_item'),
-    #delete item as user
-    path('delete/<int:id>/',views.delete_item,name='delete_item'),
-    
-    
+    # Food path
+    path('', views.IndexClassView.as_view(), name='index'),   
+    # Food details path
+    path('<int:pk>/', views.DetailFood.as_view(), name='details'),
+    path('item/', views.item, name='item'),
+    # Add items
+    path('add/', views.CreateItem.as_view(), name='create_item'),
+    # Edit items as user
+    path('update/<int:id>/', views.update_item, name='update_item'),
+    # Delete item as user
+    path('delete/<int:item_id>/', views.delete_item, name='delete_item'),
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,53 +1,77 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Item
 from django.template import loader
 from .forms import ItemForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.contrib import messages
+
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    template =loader.get_template('food/index.html')
+    template = loader.get_template('food/index.html')
     context = {
         'item_list': item_list,
     }
-    return HttpResponse(template.render(context,request))
+    return HttpResponse(template.render(context, request))
+
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
 
 def item(request):
-    return HttpResponse("<h1>welcome to tomtom</h1>")
+    return HttpResponse("<h1>Welcome to TomTom</h1>")
 
-def details(request,item_id):
-    item=Item.objects.get(pk=item_id)
+def details(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
     context = {
-        
-        'item':item,
+        'item': item,
     }
-    return render(request,'food/details.html',context)
+    return render(request, 'food/details.html', context)
+
+class DetailFood(DetailView):
+    model = Item
+    template_name = 'food/details.html'
 
 def create_item(request):
     form = ItemForm(request.POST or None)
-    
     if form.is_valid():
         form.save()
-        
+        messages.success(request, 'Item created successfully.')
         return redirect('food:index')
-    
-    return render(request,'food/item-form.html',{'form':form})
+    return render(request, 'food/item-form.html', {'form': form})
 
-def update_item(request,id):
-    item = Item.objects.get(id=id) 
+class CreateItem(CreateView):
+    model = Item
+    fields = ['item_name', 'item_desc', 'item_price', 'item_image']
+    template_name = 'food/item-form.html'
+
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+        messages.success(self.request, 'Item created successfully.')
+        self.object = form.save()
+        return redirect('food:details',pk=self.object.pk)
+
+def update_item(request, id):
+    item = get_object_or_404(Item, id=id)
     form = ItemForm(request.POST or None, instance=item)
     
     if form.is_valid():
         form.save()
+        messages.success(request, 'Item updated successfully.')
         return redirect('food:index')
-    return render(request,'food/item-form.html',{'form':form,'item':item})
+    
+    return render(request, 'food/item-form.html', {'form': form, 'item': item})
 
-def delete_item(request,id): 
-    item = Item.objects.get(id=id) 
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
     
     if request.method == 'POST':
         item.delete()
+        messages.success(request, 'Item deleted successfully.')
         return redirect('food:index')
     
-    return render(request,'food/item-delete.html',{'item':item})
-    
+    return render(request, 'food/item-delete.html', {'item': item})
